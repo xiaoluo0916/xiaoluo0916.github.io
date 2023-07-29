8943e52fd745cefed807d53d30bc0e82bec75475
xiaoluo0916 <xiao.luo.carol@gmail.com>
2023-07-28 21:51:59 -0700

    refresh

--- scripts/charts.js
+++ scripts/charts.js
@@ -1,94 +1,64 @@
 // First scene
 async function scene1() {
-  // // set the dimensions and margins of the graph
-  // const margin = {top: 10, right: 30, bottom: 30, left: 60},
-  //   width = 460 - margin.left - margin.right,
-  //   height = 400 - margin.top - margin.bottom;
+    // set the dimensions and margins of the graph
+    const margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 800 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-  // const data = await d3.csv("https://github.com/xiaoluo0916/xiaoluo0916.github.io/blob/main/data/crime%20count%20by%20date.csv");
+    // append the svg object to the body of the page
+    const svg = d3.select("#chart1")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  // const svg = d3.select("#chart1")
-  //   .append("svg")
-  //     .attr("width", width + margin.left + margin.right)
-  //     .attr("height", height + margin.top + margin.bottom)
-  //   .append("g")
-  //     .attr("transform", `translate(${margin.left},${margin.top})`);
+    //Read the data
+    d3.csv("https://raw.githubusercontent.com/xiaoluo0916/xiaoluo0916.github.io/main/data/crime%20count%20by%20date.csv",
 
-  // // Add X axis
-  // const x = d3.scaleTime()
-  //     .domain(d3.extent(data, function(d) { return d.date; }))
-  //     .range([ 0, width ]);
-  //   svg.append("g")
-  //     .attr("transform", `translate(0, ${height})`)
-  //     .call(d3.axisBottom(x));
+    // When reading the csv, I must format variables:
+    function(d){
+    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+    }).then(
 
-  // // Add Y axis
-  // const y = d3.scaleLinear()
-  //     .domain([0, d3.max(data, function(d) { return +d.value; })])
-  //     .range([ height, 0 ]);
-  //   svg.append("g")
-  //     .call(d3.axisLeft(y));
+    // Now I can use this dataset:
+    function(data) {
 
-  // // Add the line
-  // svg.append("path")
-  //     .datum(data)
-  //     .attr("fill", "none")
-  //     .attr("stroke", "steelblue")
-  //     .attr("stroke-width", 1.5)
-  //     .attr("d", d3.line()
-  //       .x(function(d) { return x(d.date) })
-  //       .y(function(d) { return y(d.value) })
-  //       )
+    // Add X axis --> it is a date format
+    const x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x));
 
-  // set the dimensions and margins of the graph
-  const margin = {top: 10, right: 30, bottom: 30, left: 60},
-  width = 1000 - margin.left - margin.right,
-  height = 800 - margin.top - margin.bottom;
+    // Add Y axis
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+  
+    // const tooltip = d3.select("#slide1")
+    //     .append("div")
+    //     .style("opacity", 0)
+    //     .attr("class", "tooltip")
+    //     .style("background-color", "black")
+    //     .style("border-radius", "5px")
+    //     .style("padding", "10")
+    //     .style("color", "white")
+    //     .style("width", "150px")
+    //     .style("height", "50px");
 
-  // append the svg object to the body of the page
-  const svg = d3.select("#chart1")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
-
-  //Read the data
-  d3.csv("https://raw.githubusercontent.com/xiaoluo0916/xiaoluo0916.github.io/main/data/crime%20count%20by%20date.csv",
-
-  // When reading the csv, I must format variables:
-  function(d){
-  return { date : d3.timeParse("%m/%d/%Y")(d.date), value : d.value }
-  }).then(
-
-  // Now I can use this dataset:
-  function(data) {
-
-  // Add X axis --> it is a date format
-  const x = d3.scaleTime()
-    .domain(d3.extent(data, function(d) { return d.date; }))
-    .range([ 0, width ]);
-  svg.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x));
-
-  // Add Y axis
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(data, function(d) { return +d.value; })])
-    .range([ height, 0 ]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
-
-  // Add the line
-  svg.append("path")
-    .datum(data)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1.5)
-    .attr("d", d3.line()
-      .x(function(d) { return x(d.date) })
-      .y(function(d) { return y(d.value) })
-      )
-
-  })
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
+    })
 }
