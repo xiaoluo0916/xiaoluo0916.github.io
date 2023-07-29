3d87108eef459a4c5bb4ae7b11b77689d80a0826
xiaoluo0916 <xiao.luo.carol@gmail.com>
2023-07-29 11:27:53 -0700

    data

--- scripts/charts.js
+++ scripts/charts.js
@@ -66,35 +66,27 @@ async function scene1() {
 // Second scene
 async function scene2() {
   // set the dimensions and margins of the graph
-  const margin = {top: 10, right: 30, bottom: 30, left: 60},
-  width = 800 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
+  const margin = {top: 10, right: 30, bottom: 200, left: 60},
+  width = 1200 - margin.left - margin.right,
+  height = 800 - margin.top - margin.bottom;
 
   // append the svg object to the body of the page
   const svg = d3.select("#chart2")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
   //Read the data
-  d3.csv("https://raw.githubusercontent.com/xiaoluo0916/xiaoluo0916.github.io/main/data/crime%20count%20by%20type%20and%20date.csv",
-
-  // When reading the csv, I must format variables:
-  function(d){
-  return { primary_type : d3.primary_type, value : d.value }
-  }).then(
-
-  // Now I can use this dataset:
-  function(data) {
+  d3.csv("https://raw.githubusercontent.com/xiaoluo0916/xiaoluo0916.github.io/main/data/crime%20count%20by%20type.csv").then( function(data) {
 
   // X axis
   const x = d3.scaleBand()
     .range([ 0, width ])
     .domain(data.map(d => d.primary_type))
     .padding(0.2);
-    svg.append("g")
+  svg.append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(d3.axisBottom(x))
     .selectAll("text")
@@ -103,9 +95,9 @@ async function scene2() {
 
   // Add Y axis
   const y = d3.scaleLinear()
-    .domain([0, 13000])
+    .domain([0, 220000])
     .range([ height, 0]);
-    svg.append("g")
+  svg.append("g")
     .call(d3.axisLeft(y));
 
   // Bars
@@ -117,5 +109,74 @@ async function scene2() {
       .attr("width", x.bandwidth())
       .attr("height", d => height - y(d.value))
       .attr("fill", "#69b3a2")
+
+  })
+}
+
+// Third scene
+async function scene3() {
+  // set the dimensions and margins of the graph
+  const margin = {top: 10, right: 30, bottom: 200, left: 60},
+  width = 1200 - margin.left - margin.right,
+  height = 800 - margin.top - margin.bottom;
+
+  // append the svg object to the body of the page
+  const svg = d3.select("#chart3")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  //Read the data
+  d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_stacked.csv").then( function(data) {
+
+    // List of subgroups = header of the csv files = soil condition here
+    const subgroups = data.columns.slice(1)
+
+    // List of groups = species here = value of the first column called group -> I show them on the X axis
+    const groups = data.map(d => (d.group))
+
+    // Add X axis
+    const x = d3.scaleBand()
+        .domain(groups)
+        .range([0, width])
+        .padding([0.2])
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x).tickSizeOuter(0));
+
+    // Add Y axis
+    const y = d3.scaleLinear()
+      .domain([0, 60])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+    // color palette = one color per subgroup
+    const color = d3.scaleOrdinal()
+      .domain(subgroups)
+      .range(['#e41a1c','#377eb8','#4daf4a'])
+
+    //stack the data? --> stack per subgroup
+    const stackedData = d3.stack()
+      .keys(subgroups)
+      (data)
+
+    // Show the bars
+    svg.append("g")
+      .selectAll("g")
+      // Enter in the stack data = loop key per key = group per group
+      .data(stackedData)
+      .join("g")
+        .attr("fill", d => color(d.key))
+        .selectAll("rect")
+        // enter a second time = loop subgroup per subgroup to add all rectangles
+        .data(d => d)
+        .join("rect")
+          .attr("x", d => x(d.data.group))
+          .attr("y", d => y(d[1]))
+          .attr("height", d => y(d[0]) - y(d[1]))
+          .attr("width",x.bandwidth())
   })
 }
