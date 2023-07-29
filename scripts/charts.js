c4598b4154d53301693edad0ad730402fec25c07
xiaoluo0916 <xiao.luo.carol@gmail.com>
2023-07-29 11:04:18 -0700

    upload data

--- scripts/charts.js
+++ scripts/charts.js
@@ -83,69 +83,39 @@ async function scene2() {
 
   // When reading the csv, I must format variables:
   function(d){
-  return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+  return { primary_type : d3.primary_type, value : d.value }
   }).then(
 
   // Now I can use this dataset:
   function(data) {
 
-  // group the data: I want to draw one line per group
-  const sumstat = d3.group(data, d => d.primary_type); // nest function allows to group the calculation per level of a factor
-
-  // // Add X axis --> it is a date format
-  // const x = d3.scaleTime()
-  //   .domain(d3.extent(data, function(d) { return d.date; }))
-  //   .range([ 0, width ]);
-  // svg.append("g")
-  //   .attr("transform", `translate(0, ${height})`)
-  //   .call(d3.axisBottom(x));
-  
-  // Add X axis --> it is a date format
-  const x = d3.scaleLinear()
-    .domain(d3.extent(data, function(d) { return d.year; }))
-    .range([ 0, width ]);
-  svg.append("g")
+  // X axis
+  const x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(data.map(d => d.primary_type))
+    .padding(0.2);
+    svg.append("g")
     .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x).ticks(5));
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
 
-  // // Add Y axis
-  // const y = d3.scaleLinear()
-  //   .domain([0, d3.max(data, function(d) { return +d.value; })])
-  //   .range([ height, 0 ]);
-  // svg.append("g")
-  //   .call(d3.axisLeft(y));
-  
   // Add Y axis
   const y = d3.scaleLinear()
-    .domain([0, d3.max(data, function(d) { return +d.value; })])
-    .range([ height, 0 ]);
-  svg.append("g")
+    .domain([0, 13000])
+    .range([ height, 0]);
+    svg.append("g")
     .call(d3.axisLeft(y));
-  
-  // color palette
-  const color = d3.scaleOrdinal()
-    .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
 
-  // const tooltip = d3.select("#slide1")
-  //     .append("div")
-  //     .style("opacity", 0)
-  //     .attr("class", "tooltip")
-  //     .style("background-color", "black")
-  //     .style("border-radius", "5px")
-  //     .style("padding", "10")
-  //     .style("color", "white")
-  //     .style("width", "150px")
-  //     .style("height", "50px");
-
-  // Add the line
-  svg.append("path")
-    .datum(data)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1.5)
-    .attr("d", d3.line()
-      .x(function(d) { return x(d.date) })
-      .y(function(d) { return y(d.value) })
-      )
+  // Bars
+  svg.selectAll("mybar")
+    .data(data)
+    .join("rect")
+      .attr("x", d => x(d.primary_type))
+      .attr("y", d => y(d.value))
+      .attr("width", x.bandwidth())
+      .attr("height", d => height - y(d.value))
+      .attr("fill", "#69b3a2")
   })
 }
